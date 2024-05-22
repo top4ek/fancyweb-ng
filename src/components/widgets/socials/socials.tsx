c73ec6fad7a02ee8a32ba65231115a2d324c3219
Yar Bash <puer.robustus@gmail.com>
2024-05-22 21:36:21 +0300

    hotfix(socials): fix component typing

--- src/components/widgets/socials/socials.tsx
+++ src/components/widgets/socials/socials.tsx
@@ -1,13 +1,10 @@
 import SocialIcons from '../../../assets/icons/social';
 import { socialContants } from './constants';
 
-type Socials = keyof typeof SocialIcons;
-type Social = { link: string, title: Socials }; 
-
 export default function Socials() {
   return (
     <ul className="flex flex-row max-w-max">
-      {socialContants.map((social: Social) => {
+      {socialContants.map((social) => {
         const Icon = SocialIcons[social.title];
         return (
           <li className="transition-colors duration-500 border border-grey-bg first:rounded-l-md last:rounded-r-md hover:border-black">
