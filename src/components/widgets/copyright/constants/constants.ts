c85a3b66fa69d44dde97896d081df4a1ab5758f7
Yar Bash <puer.robustus@gmail.com>
2024-06-07 21:10:30 +0300

    hotfix(copyright): fix year

--- src/components/widgets/copyright/constants/constants.ts
+++ src/components/widgets/copyright/constants/constants.ts
@@ -1,5 +1,5 @@
 export const copyrightConstants = {
-  text: [`Copyright 2022-${new Date().getFullYear()} `, '.'],
+  text: [`Copyright 2019-${new Date().getFullYear()} `, '.'],
   link: {
     title: 'OpenIPC',
     href: 'https://openipc.org',
