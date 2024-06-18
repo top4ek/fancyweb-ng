03104af67ebf21bae1ec8c6bf18196fbd9400319
Yar Bash <puer.robustus@gmail.com>
2024-06-18 20:46:30 +0300

    hotfix(abc,vendor -list): remove permanent scroll for necessary only

--- src/components/widgets/vendors-list/vendors-list.tsx
+++ src/components/widgets/vendors-list/vendors-list.tsx
@@ -3,6 +3,8 @@ import { vendorsListProps } from './types';
 export default function VendorsList(props: vendorsListProps) {
   const { list, curSelected, clickHandler } = props;
 
+  const isLastElem = (cur: number, arr: typeof list) => cur === arr.length - 1;
+
   const handleClick = (e: MouseEvent) => {
     const letterContainer = e.currentTarget;
     if (letterContainer instanceof HTMLLIElement) {
@@ -15,9 +17,9 @@ export default function VendorsList(props: vendorsListProps) {
   return (
     <div className="max-w-full overflow-x-auto">
       <ul className="pb-2 flex flex-row flex-nowrap text-sm text-text-blue">
-        {list.map((vendor: string) => (
+        {list.map((vendor: string, i: number) => (
           <li className={`p-[6px] border-[1px] text-nowrap border rounded-t border-transparent border-b-grey relative ${curSelected && vendor.toLowerCase() === curSelected.toLowerCase() && 'border-b-white border-x-grey border-t-grey *:border-0 hover:text-btn-blue-hover'} hover:border-b-white hover:border-x-grey hover:border-t-grey hover:cursor-pointer *:hover:border-0 hover:text-btn-blue-hover`} onClick={handleClick}>
-            <div className="w-[3px] h-[2px] border-[1px] border-transparent border-b-grey absolute -bottom-[1px] -right-[2px]"></div>
+            {!isLastElem(i, list) && <div className="w-[3px] h-[2px] border-[1px] border-transparent border-b-grey absolute -bottom-[1px] -right-[2px]"></div>}
             <span>{vendor}</span>
           </li>
         ))}
