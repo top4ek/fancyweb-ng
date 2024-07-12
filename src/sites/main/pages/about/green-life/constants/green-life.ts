7442f3052f1d8bd0cb31b835e8ed9511b3115a84
Yar Bash <puer.robustus@gmail.com>
2024-07-12 10:28:26 +0300

    feat(green-life): add Green Life page

---
+++ src/sites/main/pages/about/green-life/constants/green-life.ts
@@ -0,0 +1,7 @@
+export const greenLifeConstants = {
+  h1: 'Green Life',
+  p1: 'OpenIPC contributes to sustainability by primarily providing open firmware for IP cameras, directly engaging in the efficient use and repurposing of existing hardware.',
+  p2: 'By extending the lifespan of cameras through customizable firmware updates and features, OpenIPC reduces electronic waste.',
+  p3: 'This aligns with sustainability goals by promoting the reuse and repurposing of technology, minimizing the environmental impact associated with the production and disposal of electronic devices.',
+  p4: 'Additionally, OpenIPC\'s commitment to transparency and community-driven development fosters a collaborative approach to technology, encouraging users to actively engage and contribute to the project\'s growth and sustainability.',
+}
