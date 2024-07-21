1b15a06beac2c5c258cb25066332e1ae96d89f44
Yar Bash <puer.robustus@gmail.com>
2024-07-21 20:56:01 +0300

    feat(modal-image): Modal-image components refactor to automatically compute main content position

--- src/components/widgets/modal-image/Modal-image.tsx
+++ src/components/widgets/modal-image/Modal-image.tsx
@@ -14,25 +14,25 @@ export default function ModalImage({ src, alt, close }: { src: string, alt: stri
 
   useEffect(() => {
     document.addEventListener("keyup", handleEscKeyPress);
-    const scrollY = window.scrollY;
-    console.log('Window scroll = ', scrollY);
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scrollY}px`;
+    const innerWidth = window.innerWidth;
+    const { right: bodyRight } = document.body.getBoundingClientRect();
     document.body.style.width = '100%';
-    document.body.style.paddingRight = '12px';
+    document.body.style.paddingRight = `${innerWidth - bodyRight}px`;
+    document.body.style.top = `-${window.scrollY}px`;
+    document.body.style.position = 'fixed';
     return () => {
+      document.removeEventListener("keyup", handleEscKeyPress)
       const scrollY = document.body.style.top;
-      document.body.style.position = '';
-      document.body.style.top = '';
     document.body.style.width = '';
-      window.scrollTo(0, parseInt(scrollY || '0') * -1);
-      document.removeEventListener("keyup", handleEscKeyPress);
-    document.body.style.paddingRight = '';
+      document.body.style.position = 'static';
+      document.body.style.setProperty('padding-right', '0');
+      document.body.style.top = '';
+      window.scroll(0, parseInt(scrollY || '0') * -1);
     }
-  }, []);
+  }, [])
 
   return (
-    <div className="fixed left-0 top-0 w-full h-full overflow-x-hidden overflow-y-auto bg-[rgba(0,0,0,0.6)] z-[1500] py-8 outline-0 md:block flex flex-row items-center" onClick={handleBackdropClick}>
+    <div className="fixed left-0 top-0 w-full h-full overflow-x-hidden overflow-y-auto overscroll-y-contain bg-[rgba(0,0,0,0.6)] z-[1500] py-8 outline-0 md:block flex flex-row items-center" onClick={handleBackdropClick}>
       <div className="w-10/12 mx-auto border rounded-lg bg-white">
         <div className="flex flex-row p-4 bg-white border-b-2 rounded-t-lg">
           <p className="w-[calc(100%-24px)] truncate text-brand-blue text-lg">{alt}</p>
