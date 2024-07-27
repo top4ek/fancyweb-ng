3d5d53e003a6bfdb7acb7ff75d8169f5b9071f36
Yar Bash <puer.robustus@gmail.com>
2024-07-27 18:47:00 +0300

    build(tests): add testing mechanism vitest, add some tests

---
+++ src/utils/validators/string-validators.ts
@@ -0,0 +1,11 @@
+export function isDigitsOnly(val: string) {
+  return /^\d+$/.test(val);
+}
+
+export function isValidMAC(val: string) {
+  return /^([0-9A-Faf]{2}:){5}[0-9A-Faf]{2}$/.test(val);
+}
+
+export function isValidIP(val: string) {
+  return /^(?!0\.0\.0\.0|255\.255\.255\.255)((((2([0-4][0-9]|5[0-5]))|1[0-9]{2}|[0-9]{1,2})\.){3}(((2([0-4][0-9]|5[0-5]))|1[0-9]{2}|[0-9]{1,2})))$/.test(val);
+}
