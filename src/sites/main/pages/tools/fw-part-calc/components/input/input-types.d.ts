5b26c343953988079460b386139a2d355d465a1a
Yar Bash <puer.robustus@gmail.com>
2024-08-06 17:24:44 +0300

    hotfix(input-part-calc): add text direction

--- src/sites/main/pages/tools/fw-part-calc/components/input/input-types.d.ts
+++ src/sites/main/pages/tools/fw-part-calc/components/input/input-types.d.ts
@@ -1,6 +1,7 @@
 export type InputProps = {
   elemName: string,
   label: string,
-  borderWidth: '1px' | '3px',
+  borderWidth: '1px' | '4px',
   borderColor: 'default' | 'partition1' | 'partition2' | 'partition3' | 'partition4' | 'partition5' | 'partition6' | 'partition7' | 'partition8',
+  dir?: 'ltr' | 'rtl',
 }
