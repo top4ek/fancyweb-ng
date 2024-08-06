f25fda653bcf009f3691d6fba0bda0681601eeb5
Yar Bash <puer.robustus@gmail.com>
2024-08-06 20:51:30 +0300

    feat(fw-calc): extend firmware calc components propeties

--- src/sites/main/pages/tools/fw-part-calc/components/output/Output.tsx
+++ src/sites/main/pages/tools/fw-part-calc/components/output/Output.tsx
@@ -2,7 +2,7 @@ import { OutputProps } from './output-types';
 
 export default function Output({ label, data }: OutputProps) {
   return (
-    <div className="rounded-md flex flex-col bg-wallet-bg w-full">
+    <div className="rounded-md flex flex-col bg-wallet-bg w-full min-h-[76px]">
       <p className="text-sm text-dark-grey ml-1 mt-0.5 truncate">{label}</p>
       <p className="pl-1 py-1 m-2 font-mono text-xl h-7 text-brand-blue" dir="rtl">{data}</p>
     </div>
