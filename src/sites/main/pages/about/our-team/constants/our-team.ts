cefa3e567a63818beb597de5568b34feeb7f9172
Yar Bash <puer.robustus@gmail.com>
2024-09-03 20:26:06 +0300

    hotfix(links): remove domain

--- src/sites/main/pages/about/our-team/constants/our-team.ts
+++ src/sites/main/pages/about/our-team/constants/our-team.ts
@@ -1,7 +1,7 @@
 import type { TeamProps } from '../../../../../../components/widgets/team/team-types';
 export const ourTeamConstants = {
   h1: 'Our Team',
-  p1: 'OpenIPC is a community-driven open source project, and many people contribute to its codebase. Kudos to them! There is also a core team of skilled developers who work hard to expand the list of [supported hardware platforms](https://openipc.org/supported-hardware), extend the functionality and stability of the firmware, review submitted code, and coordinate community efforts. Get to know the team and their areas of expertise:',
+  p1: 'OpenIPC is a community-driven open source project, and many people contribute to its codebase. Kudos to them! There is also a core team of skilled developers who work hard to expand the list of [supported hardware platforms](/supported-hardware), extend the functionality and stability of the firmware, review submitted code, and coordinate community efforts. Get to know the team and their areas of expertise:',
   h2_1: 'IPC Core',
   h2_2: 'URLLC and FPV',
 }
