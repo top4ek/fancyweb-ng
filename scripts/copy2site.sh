652feb7d842ec5223ad9866f26782aeed6a1bb77
Yar Bash <puer.robustus@gmail.com>
2024-11-20 15:39:17 +0300

    test-deploy 18

--- scripts/copy2site.sh
+++ scripts/copy2site.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo $SERVER_IP
-ssh root@88.198.104.133 rm -r /var/www/webface_test
-ssh root@88.198.104.133 mkdir -p /var/www/webface_test
+# ssh root@88.198.104.133 rm -r /var/www/webface_test
+# ssh root@88.198.104.133 mkdir -p /var/www/webface_test
