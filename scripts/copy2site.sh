6ce055c426b90dac612265031d4c9d315313ce60
Yar Bash <puer.robustus@gmail.com>
2024-11-21 21:27:39 +0300

    test-deploy 27

--- scripts/copy2site.sh
+++ scripts/copy2site.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-echo "Server name: $SERVER_NAME"
-ssh root@${SERVER_NAME} rm -r /var/www/webface_test
-ssh root@${SERVER_NAME} mkdir -p /var/www/webface_test
+rm -r /var/www/webface_test
+mkdir -p /var/www/webface_test
